49876daccc5d8735493a2d681e15bc32373e1b3b
mavithaalochana <131955437+mavithaalochana@users.noreply.github.com>
2025-02-23 12:12:29 +0530

    Add files via upload

---
+++ controller/Alogin.java
@@ -0,0 +1,51 @@
+package com.datapackage.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.datapackage.dao.AdminloginDAO;
+
+/**
+ * Servlet implementation class Alogin
+ */
+@WebServlet("/Alogin")
+public class Alogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Alogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        AdminloginDAO adminloginDAO = new AdminloginDAO();
+        
+        if (adminloginDAO.authenticateAdmin(email, password)) {
+            response.sendRedirect("admin.jsp");
+        } else {
+            response.getWriter().write("fail");
+        }
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
