c5978d94545bee2d919b5795d3e63445fab3f594
mavithaalochana <131955437+mavithaalochana@users.noreply.github.com>
2025-02-25 18:55:19 +0530

    Add files via upload

---
+++ model/Dlogin.java
@@ -0,0 +1,40 @@
+package com.datapackage.model;
+
+/**
+ * Represents the driver login entity
+ */
+public class Dlogin {
+    private int id;
+    private String email;
+    private String password;
+
+    public Dlogin(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
