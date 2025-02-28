ee5e6269b8e2ff9e511f9cbedd05f9391a3e5423
mavithaalochana <131955437+mavithaalochana@users.noreply.github.com>
2025-02-28 18:53:36 +0530

    Add files via upload

---
+++ controller/LoginServlet.java
@@ -0,0 +1,52 @@
+package com.datapackage.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.datapackage.dao.LoginDAO;
+import com.datapackage.model.Login;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        Login login = new Login(email, password);
+        LoginDAO loginDAO = new LoginDAO();
+
+        if (loginDAO.authenticateUser(login)) {
+            response.sendRedirect("Menu.jsp");
+        } else {
+            response.getWriter().write("fail");
+        }
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
